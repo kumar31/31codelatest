a3314f1c4d300b2372e3b54df74f4e0c1ed56ea5
kumar31 <kumarappan@smaatapps.com>
2016-08-30 14:34:15 +0530

    Update Login.php

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 error_reporting(0);
-class login extends CI_Controller {
+class Login extends CI_Controller {
 
 public function __construct()
 	{
