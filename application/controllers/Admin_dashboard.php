a7695ccdd8439abda121c88a4fcd7217a9e7d1ab
kumar <kumarappan@smaatapps.com>
2016-08-30 18:33:09 +0530

    website controller renamed

---
+++ application/controllers/Admin_dashboard.php
@@ -0,0 +1,46 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+error_reporting(getenv( 'SOIREE_ERROR_REPORTING' ));
+class Admin_dashboard extends CI_Controller {
+
+public function __construct()
+	{
+	
+			parent::__construct();
+			$this->load->helper('url');
+			$this->load->helper('form');
+			$this->load->helper('cookie');
+			$this->load->library('form_validation');
+			
+	
+	}
+	public function index()
+	{
+		if($_POST['my_userid'] == "") {
+			$myuser_id = $this->session->userdata('admin_id'); 
+			if($myuser_id == ''){
+					$myuser_id = $this->input->cookie('admin',true);
+				}
+		}
+		else {
+			$adminid = $_POST['my_userid']; 
+			$userdata = array(
+					   'admin_id'  => $adminid,
+			);
+			$this->session->set_userdata($userdata);
+			$myuser_id = $this->session->userdata('admin_id');
+			$cookie= array(
+				'name'   => 'admin',
+				'value'  => $myuser_id,
+				'expire' => '86500'
+			);
+			$this->input->set_cookie($cookie);
+		}
+		if($myuser_id == "") {
+			redirect('admin_login');
+		}
+		else{
+			$this->load->view('admin_dashboard',$myuser_id);
+		}
+	}
+	
+}
---
+++ application/controllers/Client_dashboard.php
@@ -0,0 +1,48 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+error_reporting(getenv( 'SOIREE_ERROR_REPORTING' ));
+class Client_dashboard extends CI_Controller {
+
+public function __construct()
+	{
+	
+			parent::__construct();
+			$this->load->helper('url');
+			$this->load->helper('cookie');
+			$this->load->helper('form');
+			$this->load->library('form_validation');
+			
+	
+	}
+	public function index()
+	{
+		if($_POST['my_userid'] == "") {
+			$myuser_id = $this->session->userdata('client_id'); 
+			if($myuser_id == ''){
+					$myuser_id = $this->input->cookie('client',true);
+				}
+		}
+		//$clientid = $this->uri->segment(2); 
+		else {
+			$clientid = $_POST['my_userid']; 
+			$userdata = array(
+				'client_id'  => $clientid,
+			);
+			$this->session->set_userdata($userdata);
+			$myuser_id = $this->session->userdata('client_id');
+			 $cookie= array(
+				'name'   => 'client',
+				'value'  => $myuser_id,
+				'expire' => '86500'
+			);
+			$this->input->set_cookie($cookie);
+		}
+		if($myuser_id == "") {
+			redirect('login');
+		}
+		else{
+			$this->db->truncate('fb');
+			$this->load->view('client_dashboard');
+		}
+	}
+	
+}
