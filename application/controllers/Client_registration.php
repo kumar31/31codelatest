ed386c64dea7bac9624a0f35e901ddaac505a6a7
kumar31 <kumarappan@smaatapps.com>
2016-08-30 14:44:48 +0530

    Update and rename client_registration.php to Client_registration.php

--- application/controllers/Client_registration.php
+++ application/controllers/Client_registration.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 error_reporting(0);
-class client_registration extends CI_Controller {
+class Client_registration extends CI_Controller {
 
 public function __construct()
 	{
