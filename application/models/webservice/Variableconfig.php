7c00405663d5200eb490308a18ef78e44308ff75
kumar31 <kumarappan@smaatapps.com>
2016-08-31 13:20:41 +0530

    webservice

---
+++ application/models/webservice/Variableconfig.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+date_default_timezone_set("GMT");
+class Variableconfig extends CI_Model {
+	public function __construct()
+	{
+		parent::__construct(); 
+	}
+	
+	
+	function smsurl() {
+			return getenv( 'SOIREE_BASE_URL' ) . '/twilio/Services/sms.php';
+	}
+	
+	function events_smsurl() {
+			return getenv( 'SOIREE_BASE_URL' ) . '/twilio/Services/sms_events.php';
+	}
+	
+}
+	
