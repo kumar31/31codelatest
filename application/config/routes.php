8830baba183a51677ae65b00f54714c2032909c4
kumar <kumarappan@smaatapps.com>
2016-08-31 12:04:05 +0530

    chk

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+
+$route['client_dashboard/(:num)'] = "client_dashboard"; 
+$route['talent_dashboard/(:num)'] = "talent_dashboard"; 
+$route['talent_invited_view/(:num)'] = "talent_invited_view"; 
+$route['myevents_client/(:num)'] = "myevents_client"; 
+$route['myevents_client_closed/(:num)'] = "myevents_client_closed"; 
+$route['client_proposals_pending/(:num)'] = "client_proposals_pending"; 
+$route['client_proposals_hired/(:num)'] = "client_proposals_hired"; 
+$route['event_detail/(:num)'] = "event_detail"; 
+$route['edit_event/(:num)'] = "edit_event"; 
+$route['event_detail_talent/(:num)'] = "event_detail_talent"; 
+$route['pending_events_talent/(:num)'] = "pending_events_talent"; 
+$route['event_detail_talent_invited/(:num)'] = "event_detail_talent_invited"; 
+$route['event_detail_talent_pending/(:num)'] = "event_detail_talent_pending"; 
+$route['event_detail_talent_confirmed/(:num)'] = "event_detail_talent_confirmed"; 
+$route['client_proposals_applied/(:num)'] = "client_proposals_applied"; 
+$route['talent_detail/(:any)'] = "talent_detail"; 
+$route['talent_fill_timesheet/(:num)'] = "talent_fill_timesheet"; 
+$route['client_verify_timesheet/(:num)'] = "client_verify_timesheet"; 
+$route['client_timesheet_check/(:num)'] = "client_timesheet_check"; 
+$route['project_details/(:num)'] = "project_details"; 
+$route['event_detail/(:any)'] = "event_detail"; 
+$route['manage_events/(:num)'] = "manage_events"; 
+$route['management/(:num)'] = "management"; 
+$route['event_detail_talent_closed/(:num)'] = "event_detail_talent_closed"; 
+$route['client_update_payment_details/(:num)'] = "client_update_payment_details"; 
+$route['talent_update_payment_details/(:num)'] = "talent_update_payment_details"; 
+$route['invite_talent_search/(:num)'] = "invite_talent_search"; 
+$route['admin_dashboard/(:num)'] = "admin_dashboard"; 
+$route['event_type/(:num)'] = "event_type"; 
+$route['agent_dashboard/(:num)'] = "agent_dashboard"; 
+$route['agent_profile/(:num)'] = "agent_profile"; 
+$route['agent_talents/(:num)'] = "agent_talents"; 
+$route['admin_settings/(:num)'] = "admin_settings"; 
+$route['contractor_pay/(:num)'] = "contractor_pay"; 
