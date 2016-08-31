3b5f21cf768a3c4b7a3fe8a5bef7138163264539
kumar31 <kumarappan@smaatapps.com>
2016-08-31 12:00:04 +0530

    models

---
+++ webservice/application/models/Client_update_password_model.php
@@ -0,0 +1,26 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+date_default_timezone_set("GMT");
+class Client_update_password_model extends CI_Model {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('client_model');
+		$this->load->library('encrypt');
+	}
+	
+	
+	
+	function index()
+	{
+		$password = $this->encrypt->encode($_POST['new_password']); 
+		$data = array(
+			'password' 			=> $password
+		);
+		
+		$this->db->where('client_id',$_POST['client_id']);
+		$this->db->update('client_details',$data);
+	
+	}
+	
+}
+	
---
+++ webservice/application/models/Get_agent_details_model.php
@@ -0,0 +1,103 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+date_default_timezone_set("GMT");
+class Get_agent_details_model extends CI_Model {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->library('encrypt');
+	}
+	
+	
+	
+	function index()
+	{		
+		
+			$this->db->select('*');		
+			$this->db->from('agent_details');
+			$query = $this->db->get();
+			$result = $query->result_array(); 
+			return $result;
+			
+		
+	}
+	function edit()
+	{		
+		
+			$this->db->select('*');		
+			$this->db->where('agent_id',$_POST['agent_id']);
+			$this->db->from('agent_details');
+			$query = $this->db->get();
+			$result = $query->result_array(); 
+			return $result;
+			
+		
+	}
+	
+	function active_inactive(){
+		
+		$agent = $this->edit();
+		if(!empty($agent)){
+			$agent_status = $agent[0]['status'];
+			if($agent_status == 1){
+				
+				$result = array('status'=>'2');
+				
+			}else{
+				
+				$result = array('status'=>'1');
+				
+			}
+			$this->db->where('agent_id',$_POST['agent_id']);
+			$this->db->update('agent_details',$result);
+		}else{
+			$result = array('status'=>'3');
+		}
+		return $result;
+	}
+	
+	function update(){
+		if($_POST['password'] == "") {
+			$this->db->select('*');		
+			$this->db->where('agent_id',$_POST['agent_id']);
+			$this->db->from('agent_details');
+			$query = $this->db->get();
+			$result = $query->result_array(); 
+			$password = $result[0]['password'];
+		}
+		else{ 
+			$password = $this->encrypt->encode($_POST['password']);
+		}
+		
+		$data = array(
+		'name'      => $_POST['name'],
+		'email'		=> $_POST['email'],
+		'password' 	=> $password,
+		'percentage'=> $_POST['percentage'],
+		'address'	=> $_POST['address'],
+		'outfit_fee'=> $_POST['outfit_fee']
+		);
+		$this->db->where('agent_id',$_POST['agent_id']);
+		$this->db->update('agent_details',$data);
+		if($this->db->affected_rows() > 0){
+			return "success";
+		}else{
+			return "error";
+		}
+	}
+	function create(){
+		$password = $this->encrypt->encode($_POST['password']);
+		$data = array(
+		'name'      => $_POST['name'],
+		'email'		=> $_POST['email'],
+		'password' 	=> $password,
+		'percentage'=> $_POST['percentage'],
+		'address'	=> $_POST['address'],
+		'outfit_fee'=> $_POST['outfit_fee'],
+		'status'	=> 1,
+		);
+		$this->db->insert('agent_details',$data);
+		
+	}
+	
+}
+	
