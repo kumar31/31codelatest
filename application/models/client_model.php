c464acd309147b351b029ee4de0d9e4785666ecd
kumar <kumarappan@smaatapps.com>
2016-08-31 10:07:38 +0530

    chk

--- application/models/client_model.php
+++ application/models/client_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 date_default_timezone_set("UTC");
-class client_model extends CI_Model {
+class Client_model extends CI_Model {
 	public function __construct()
 	{
 		parent::__construct(); 
