9d3810dd9220e8abc6d99bb05774156461059764
kumar <kumarappan@smaatapps.com>
2016-09-01 18:49:09 +0530

    chk

--- application/views/talent_support.php
+++ application/views/talent_support.php
@@ -52,7 +52,7 @@ $this->load->model('webservice/mail_model','mail_model'); ?>
 			$message = "<p>Email: ".$email." <br> First name: ".$fname." <br>Last name: ".$lname." <br> Message: ".$msg."</p>";
 			
 			$to_email = "car3chan@gmail.com";
-			$this->mail_model->email($to_email,$subject,$message); 
+			$this->mail_model->send($to_email,$subject,$message); 
 			
 		  }
 		  ?>
