07530ff139b7f540ee62c5166483acb8b8c5d57d
kumar <kumarappan@smaatapps.com>
2016-09-02 17:32:51 +0530

    chk

--- html2pdf_v4.03/examples/test.php
+++ html2pdf_v4.03/examples/test.php
@@ -22,7 +22,7 @@ $AdID=$_GET['event_id'];
   
     ob_start();
    
-	include('res/test.php'); echo "hi"; die; 
+	require_once('res/test.php'); echo "hi"; die; 
     $content = ob_get_clean(); 
 
     // convert in PDF
