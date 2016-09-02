a177ab0d16c7071ea983d81f8995485ab038bc3b
kumar <kumarappan@smaatapps.com>
2016-09-02 18:20:46 +0530

    chk

--- html2pdf_v4.03/examples/adv.php
+++ html2pdf_v4.03/examples/adv.php
@@ -1,17 +1,11 @@
 <?php
 
-/*$AdID=$_GET['event_id']; 
-/
+	/*$AdID=$_GET['event_id'];
+
     ob_start();
    
-	include(dirname(__FILE__).'/res/adv.php'); 
-    $content = "
-<page>
-    <h1>Exemple d'utilisation</h1>
-    <br>
-    Ceci est un <b>exemple d'utilisation</b>
-    de <a href='http://html2pdf.fr/'>HTML2PDF</a>.<br>
-</page>";
+	include(dirname(__FILE__).'/res/adv.php');
+    $content = ob_get_clean();
 
     // convert in PDF
     require_once(dirname(__FILE__).'/../html2pdf.class.php');
@@ -19,13 +13,11 @@
     {
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
 //      $html2pdf->setModeDebug();
-        $html2pdf->setDefaultFont('Arial'); 
-		ob_get_clean();
-        $html2pdf->writeHTML($content);  
-		//ob_flush();
+        $html2pdf->setDefaultFont('Arial');
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml'],$AdID));
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
-       // $html2pdf->Output("invoice.pdf");
-		$path='../../invoice/adv'.$AdID.'.pdf';		
+       // $html2pdf->Output("invoice.pdf"); 
+		$path='../../invoice/adv'.$AdID.'.pdf';
 		$html2pdf->Output($path, 'F');
 		$html2pdf->Output("adv.pdf");
 		 
@@ -33,7 +25,7 @@
     catch(HTML2PDF_exception $e) {
         echo $e;
         exit;
-    } */
+    }*/
 	require 'pdfcrowd.php';
 	require('config.php');
 	$AdID=$_GET['event_id'];	 
