9b9fc578c4f5b2764a1812e96426166fcb05b317
kumar <kumarappan@smaatapps.com>
2016-09-02 11:23:10 +0530

    chk

--- application/views/talent_support.php
+++ application/views/talent_support.php
@@ -1,7 +1,7 @@
 <?php 
 error_reporting(getenv( 'SOIREE_ERROR_REPORTING' ));
 include('talent_header.php');
-require APPPATH.'/libraries/class.phpmailer.php'; ?>
+$this->load->model('webservice/mail_model','mail_model'); ?>
   <body>
     <div class="orangehead">
       <div class="container">
@@ -52,38 +52,7 @@ require APPPATH.'/libraries/class.phpmailer.php'; ?>
 			$message = "<p>Email: ".$email." <br> First name: ".$fname." <br>Last name: ".$lname." <br> Message: ".$msg."</p>";
 			
 			$to_email = "car3chan@gmail.com";
-			$mail             = new PHPMailer();
-    	$body             = $message;
-    	$body             = eregi_replace("[\]",'',$body);
-    	$mail->IsSMTP(); // telling the class to use SMTP
-		$SMTP_HOST = getenv( 'SMTP_HOST' );
-		$SMTP_USERNAME = getenv( 'SMTP_USERNAME' );
-		$SMTP_PASSWORD = getenv( 'SMTP_PASSWORD' );
-		$SMTP_FROM = getenv( 'SMTP_FROM' );
-    	$mail->Host       = $SMTP_HOST; // SMTP server
-    	//$mail->SMTPDebug  = 2;           // enables SMTP debug information (for testing)
-    	                                 // 1 = errors and messages
-    	                                 // 2 = messages only
-    	$mail->SMTPAuth   = true;        // enable SMTP authentication
-    	$mail->Host       = $SMTP_HOST; // sets the SMTP server
-    	$mail->Port       = 25;                    // set the SMTP port for the GMAIL server
-    	$mail->Username   = $SMTP_USERNAME; // SMTP account username
-    	$mail->Password   = $SMTP_PASSWORD;        // SMTP account password
-    	$mail->SetFrom($SMTP_FROM, 'outfitstaff');
-    	$mail->AddReplyTo($SMTP_FROM,"outfitstaff");
-    	$mail->Subject    = $subject;
-    	$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-    	$mail->MsgHTML($body);
-    	$address = $to_email;
-    	$mail->AddAddress($address, "outfitstaff");
-    	//$mail->AddAttachment("images/phpmailer.gif");      // attachment
-    	//$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-    	$mail->Send();
-    	/*if(!$mail->Send()) {
-    	echo "Mailer Error: " . $mail->ErrorInfo;
-    	} else {
-    	echo "Message sent!";
-    	}*/
+			$this->mail_model->send($to_email,$subject,$message); 
 			
 		  }
 		  ?>
