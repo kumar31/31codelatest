3109b607f5250fe062a74f5a5cc611cb4a1d78c3
kumar <kumarappan@smaatapps.com>
2016-09-02 17:40:44 +0530

    chk

--- html2pdf_v4.03/examples/test.php
+++ html2pdf_v4.03/examples/test.php
@@ -25,13 +25,13 @@ $AdID=$_GET['event_id'];
     $content = ob_get_clean();
 
     // convert in PDF
-    require_once(dirname(__FILE__).'/../html2pdf.class.php');echo "hi"; die;
+    require_once(dirname(__FILE__).'/../html2pdf.class.php');
     try
     {
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
 //      $html2pdf->setModeDebug();
         $html2pdf->setDefaultFont('Arial');
-        $html2pdf->writeHTML($content, isset($_GET['vuehtml'],$AdID));
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml'],$AdID));echo "hi"; die;
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
        // $html2pdf->Output("invoice.pdf"); 
 		$path='../../invoice/adv'.$AdID.'.pdf';
