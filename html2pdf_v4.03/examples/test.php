1a501a3278f1930799f491d0bcd96ec93c074e17
kumar <kumarappan@smaatapps.com>
2016-09-02 13:00:20 +0530

    chk

--- html2pdf_v4.03/examples/test.php
+++ html2pdf_v4.03/examples/test.php
@@ -1,17 +1,30 @@
 <?php
 
-	$AdID=$_GET['event_id']; 
-/
+
+
+
+
+$AdID=$_GET['event_id'];
+//$currency=$_GET['currency'];	
+			
+
+/**
+ * HTML2PDF Librairy - example
+ *
+ * HTML => PDF convertor
+ * distributed under the LGPL License
+ *
+ * @author      Laurent MINGUET <webmaster@html2pdf.fr>
+ *
+ * isset($_GET['vuehtml']) is not mandatory
+ * it allow to display the result in the HTML format
+ */
+  
+    // get the HTML http://smaatapps.com/Myshul/website_dev/index.php/invoicewithtax/index/3/
     ob_start();
    
-	include(dirname(__FILE__).'/res/test.php'); 
-    $content = "
-<page>
-    <h1>Exemple d'utilisation</h1>
-    <br>
-    Ceci est un <b>exemple d'utilisation</b>
-    de <a href='http://html2pdf.fr/'>HTML2PDF</a>.<br>
-</page>";
+	include(dirname(__FILE__).'/res/test.php');
+    $content = ob_get_clean();
 
     // convert in PDF
     require_once(dirname(__FILE__).'/../html2pdf.class.php');
@@ -19,13 +32,11 @@
     {
         $html2pdf = new HTML2PDF('P', 'A4', 'fr');
 //      $html2pdf->setModeDebug();
-        $html2pdf->setDefaultFont('Arial'); 
-		ob_get_clean();
-        $html2pdf->writeHTML($content);  
-		//ob_flush();
+        $html2pdf->setDefaultFont('Arial');
+        $html2pdf->writeHTML($content, $AdID));
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
-       // $html2pdf->Output("invoice.pdf");
-		$path='../../invoice/adv'.$AdID.'.pdf';		
+       // $html2pdf->Output("invoice.pdf"); 
+		$path='../../invoice/adv'.$AdID.'.pdf';
 		$html2pdf->Output($path, 'F');
 		$html2pdf->Output("adv.pdf");
 		 
@@ -33,6 +44,4 @@
     catch(HTML2PDF_exception $e) {
         echo $e;
         exit;
-    } 
-	
-?>
+    }
