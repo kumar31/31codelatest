a22718598c75429317636e404cd59f640bc5a18b
kumar <kumarappan@smaatapps.com>
2016-09-02 17:49:44 +0530

    chk

--- html2pdf_v4.03/examples/test.php
+++ html2pdf_v4.03/examples/test.php
@@ -34,7 +34,7 @@ $AdID=$_GET['event_id'];
         $html2pdf->writeHTML($content);
 		$my_string = substr(str_shuffle(MD5(microtime())), 0, 6); 
        // $html2pdf->Output("invoice.pdf"); 
-		$path='../../invoice/adv'.$AdID.'.pdf';
+		$path='adv'.$AdID.'.pdf';
 		ob_clean();
 		$html2pdf->Output($path, 'F');
 		$html2pdf->Output("adv.pdf");
