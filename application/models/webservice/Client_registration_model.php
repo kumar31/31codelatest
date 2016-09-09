6e8e3f8a4a12415c5b7a7af1d35c5fdd97616508
kumar <kumarappan@smaatapps.com>
2016-09-09 10:26:04 +0530

    chk

--- application/models/webservice/Client_registration_model.php
+++ application/models/webservice/Client_registration_model.php
@@ -22,9 +22,14 @@ class Client_registration_model extends CI_Model {
 			$login_type = "facebook";
 		}
 		else {
-			$login_type = ""; 
+			$login_type = "";
 		}
-		$password = $this->encrypt->encode($_POST['password']); 
+		
+		$intermediateSalt = md5(sha1(getenv( 'SOIREE_PASSWORD_ENCRYPTION' )));
+		$salt = $intermediateSalt;
+		$password =  hash("sha256", $_POST['password'] . $salt);
+		
+		//$password = $this->encrypt->encode($_POST['password']); 
 		$data = array(
 			'email' 	   		=> $_POST['email'],
 			'password' 			=> $password,
