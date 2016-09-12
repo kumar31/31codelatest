20e0b7423e4e2097f00a0db145a4d3c6ed39f446
kumar <kumarappan@smaatapps.com>
2016-09-12 12:47:23 +0530

    chk

--- footer.php
+++ footer.php
@@ -2,6 +2,20 @@
 
 <!-- jQuery  -->
         <script src="<?php echo base_url(); ?>assets/js/jquery.min.js"></script>
+		
+		 <footer class="footer text-right">
+				<table width="100%" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose GeoTrust SSL for secure e-commerce and confidential communications.">
+<tr>
+<td width="135" align="center" valign="top"><script type="text/javascript" src="https://seal.geotrust.com/getgeotrustsslseal?host_name=www.purposegateway.com&amp;size=M&amp;lang=en"></script>
+<a href="http://www.geotrust.com/ssl/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 7px verdana,sans-serif; letter-spacing:.5px; text-align:center; margin:0px; padding:0px;"></a>
+
+<img src="https://www.purposegateway.com/image/pay_logos.png" width="250" height="67" /></td>
+</tr>
+</table>
+				<img src="<?php echo base_url(); ?>assets/images/seclogo.png" style="height: 35px;">&nbsp;&nbsp;<strong>Copyright © 2016 <a href="#">Purpose Financial LLC</a>.</strong> All rights
+    reserved.
+</footer>
+
         <script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/detect.js"></script>
         <script src="<?php echo base_url(); ?>assets/js/fastclick.js"></script>
@@ -30,18 +44,7 @@
         <script type="text/javascript" src="<?php echo base_url(); ?>assets/plugins/jquery-validation/dist/jquery.validate.min.js"></script>
 		
 		<script src="<?php echo base_url(); ?>assets/pages/jquery.wizard-init.js" type="text/javascript"></script>
-		 <footer class="footer text-right">
-				<table width="100%" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose GeoTrust SSL for secure e-commerce and confidential communications.">
-<tr>
-<td width="135" align="center" valign="top"><script type="text/javascript" src="https://seal.geotrust.com/getgeotrustsslseal?host_name=www.purposegateway.com&amp;size=M&amp;lang=en"></script>
-<a href="http://www.geotrust.com/ssl/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 7px verdana,sans-serif; letter-spacing:.5px; text-align:center; margin:0px; padding:0px;"></a>
-
-<img src="https://www.purposegateway.com/image/pay_logos.png" width="250" height="67" /></td>
-</tr>
-</table>
-				<img src="<?php echo base_url(); ?>assets/images/seclogo.png" style="height: 35px;">&nbsp;&nbsp;<strong>Copyright © 2016 <a href="#">Purpose Financial LLC</a>.</strong> All rights
-    reserved.
-</footer>
+		
 		
 		
   <!-- /.content-wrapper -->
