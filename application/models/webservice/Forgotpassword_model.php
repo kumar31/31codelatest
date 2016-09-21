f2e0ae5028c1b50ffbd3993356d7790a47393bab
kumar <kumarappan@smaatapps.com>
2016-09-21 18:19:40 +0530

    chk

--- application/models/webservice/Forgotpassword_model.php
+++ application/models/webservice/Forgotpassword_model.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 error_reporting(0);
-
+date_default_timezone_set("GMT");
 require_once APPPATH.'/libraries/variableconfig.php';
 
 class Forgotpassword_model extends CI_Model {
@@ -16,6 +16,10 @@ class Forgotpassword_model extends CI_Model {
 			$fpassurl = $variableconfig->fpassurl(); 
 			$from_email = $variableconfig->from_email();
 			
+			$rand_num = rand(000000,999999);
+			
+			$dateFormat="Y-m-d H:i:s";
+			$timeNdate=gmdate($dateFormat, time());	
 			if($_POST['type'] == 1) {
 				$this->db->select('*');		
 				$this->db->where('email',$_POST['email']);		
@@ -23,6 +27,7 @@ class Forgotpassword_model extends CI_Model {
 				$query = $this->db->get();
 				$result = $query->result_array();
 				$userid = $result[0]['client_id'];
+				
 			}
 			if($_POST['type'] == 2) {
 				$this->db->select('*');		
@@ -31,8 +36,17 @@ class Forgotpassword_model extends CI_Model {
 				$query = $this->db->get();
 				$result = $query->result_array();
 				$userid = $result[0]['talent_id'];
+				
 			}
-		
+			
+			$data = array(
+				'user_id' 				=> $userid,
+				'opaque_id' 			=> $rand_num,
+				'timestamp' 			=> $timeNdate,
+				'type' 					=> $_POST['type'],
+			);
+			$this->db->insert('password_reset_requests',$data);	
+			
 			$to_email = $result[0]['email'];
 			$firstname = $result[0]['first_name']; 
 			$password = $result[0]['password'];
@@ -40,7 +54,7 @@ class Forgotpassword_model extends CI_Model {
 			$message = "<p>Hi ".$firstname.",</p>";
 			$message .= "<p>You have requested recovery of your Outfit password.</p> </br>";
 			$message .= "<p>Click the button below and reset the password.</p> </br>";
-			$message .= "<a href=".$fpassurl.$userid."/".$_POST['type']."><button>Reset Password</button></a>";
+			$message .= "<a href=".$fpassurl.$rand_num"><button>Reset Password</button></a>";
 			$message .= "<p>If you did not request recovery of password, please ignore this mail. </br></p>";
 			$message .= "<p>Thanks for using Outfit.</p>";
 			$message .= "<p>Sincerely,</p>";
